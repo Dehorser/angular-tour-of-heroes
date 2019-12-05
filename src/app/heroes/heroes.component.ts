feed48c54e50ffbdaa28138108e04342cf12e3c9
Yuzhou Huang <happyjoe2008@gmail.com>
2019-12-04 19:06:50 -0800

    Add separate dashboard view using routing

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -8,8 +8,6 @@ import { HeroService } from '../hero.service';
   styleUrls: ['./heroes.component.css']
 })
 export class HeroesComponent implements OnInit {
-
-  selectedHero: Hero;
   heroes: Hero[];
 
   constructor(
@@ -20,9 +18,5 @@ export class HeroesComponent implements OnInit {
     this.heroService.getHeroes().subscribe(heroes =>
       this.heroes = heroes
     );
-  }
-
-  onSelect(hero: Hero): void {
-    this.selectedHero = hero;
   }
 }
