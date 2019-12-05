e303574409d067fd59c726dde15b3e8dfd4dc9b9
Yuzhou Huang <happyjoe2008@gmail.com>
2019-12-04 19:28:17 -0800

    Mock HTTP client
    this will allow for more realistic data fetches

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -1,29 +1,59 @@
 import { Injectable } from '@angular/core';
 
-import {Observable, of} from 'rxjs';
+import { Observable, of } from 'rxjs';
+import { catchError, tap } from 'rxjs/operators';
 
-import {Hero} from './hero';
-import {HEROES} from './mock-heroes';
-import {MessageService} from './message.service';
+import { Hero } from './hero';
+import { MessageService } from './message.service';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class HeroService {
 
+  private heroesUrl = 'api/heroes';  // URL to web api
+
+
+  private log(message: string) {
+    this.messageService.add(`HeroService: ${message}`);
+  }
+
+  private handleError<T>(
+    operation = 'operation', 
+    result?: T) {
+      return (error: any): Observable<T> => {
+        // TODO: send the error to remote logging infrastructure
+        console.error(error); // log to console instead
+
+        // TODO: better job of transforming error for user consumption
+        this.log(`${operation} failed: ${error.message}`);
+
+        // Let the app keep running by returning an empty result.
+        return of(result as T);
+      }
+  }
+
   constructor(
-    private messageService: MessageService
+    private messageService: MessageService,
+    private http: HttpClient
   ) { }
 
   getHeroes(): Observable<Hero[]> {
     this.messageService.add('HeroService, fetched heroes');
-    return of(HEROES);
+    return this.http.get<Hero[]>(this.heroesUrl)
+    .pipe(
+      tap(_ => this.log('fetched heroes')),
+      catchError(this.handleError<Hero[]>('getHeroes', []))
+    );
   }
 
+  /** GET hero by id. Will 404 if id not found */
   getHero(id: number): Observable<Hero> {
-    this.messageService.add(
-      `HeroService: fetched hero id=${id}`
+    const url = `${this.heroesUrl}/${id}`;
+    return this.http.get<Hero>(url).pipe(
+      tap(_ => this.log(`fetched hero id=${id}`)),
+      catchError(this.handleError<Hero>(`getHero id=${id}`))
     );
-    return of(HEROES.find(hero => hero.id === id));
   }
 }
